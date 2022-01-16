3464394657342e05e4f1a5785452c908914939bc
Tester Siladi <testersiladi@gmail.com>
2022-01-16 22:41:21 +0100

    const repo name replaced with env variable

--- src/components/summary/CountrySummary.vue
+++ src/components/summary/CountrySummary.vue
@@ -14,15 +14,15 @@
 </template>
 
 <script>
-const repoName = 'covid-app';
-
 export default {
   name: 'CountrySummary',
   props: ['countrySummary'],
   methods: {
     openCountryPerDay(countrySummaryObj) {
       this.$store.dispatch('setSelectedCountry', countrySummaryObj);
-      this.$router.push(`/${repoName}/perday/${countrySummaryObj.Slug}`);
+      this.$router.push(
+        `/${process.env.VUE_APP_REPO_NAME}/perday/${countrySummaryObj.Slug}`,
+      );
     },
   },
 };
