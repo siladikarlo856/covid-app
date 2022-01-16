57f4ef8830e5dbe8dfc994b4f5d35505af7e54d8
Tester Siladi <testersiladi@gmail.com>
2022-01-16 22:47:38 +0100

    Title changed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/covid-app/' : '/',
+  pages: {
+    index: {
+      // Entry point for the web application.
+      entry: 'src/main.js',
+      // Title - text in web browser tab.
+      title: 'Covid App',
+    },
+  },
 };
